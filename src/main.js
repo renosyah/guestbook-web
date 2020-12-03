8556fa69edbac042055c9a67921986546ff33bf1
renosyah <syahputrareno975@gmail.com>
2020-12-03 13:17:11 +0700

    add vue axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
